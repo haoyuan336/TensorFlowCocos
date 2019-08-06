2ed4fd9dcd452937facede5be18b6c1f380df57b
楚浩远 <>
2019-08-06 19:34:39 +0800

    继续学习TensorFlow

--- TensorFlowClient/assets/MainNode.js
+++ TensorFlowClient/assets/MainNode.js
@@ -6,16 +6,49 @@ cc.Class({
 
 
     onLoad() {
-        const model = tf.sequential();
-        model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-        model.compile({ loss: "meanSquaredError", optimizer: 'sgd' });
-        const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-        const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-        model.fit(xs, ys, { epochs: 10 })
-            .then(() => {
-                model.predict(tf.tensor2d([5], [1, 1])).print()
-            }); 0
-        
+        const a = tf.variable(tf.scalar(Math.random()));
+        const b = tf.variable(tf.scalar(Math.random()));
+        const c = tf.variable(tf.scalar(Math.random()));
+        const d = tf.variable(tf.scalar(Math.random()));
+        function predict(x) {
+            x.print();
+            return tf.tidy(() => {
+                let y  = a.mul(x.pow(tf.scalar(3)))
+                    .add(b.mul(x.square()))
+                    .add(c.mul(x))
+                    .add(d);
+                    y.print();
+                    return y;
+            });
+        }
+        function loss(predictions, labels) {
+            const meanSquareErroe = predictions.sub(labels).square().mean();
+            return meanSquareErroe;
+        }
+        function train(xs, ys, numInterations = 75) {
+            const learningRate = 0.5;
+            const optimizer = tf.train.sgd(learningRate);
+            for (let i = 0; i < numInterations; i++) {
+                optimizer.minimize(() => {
+                    const predsYs = predict(xs);
+                    return loss(predsYs, ys);
+                })
+            }
+        }
+
+        function getY(x) {
+            return 10 * Math.pow(x, 3) + 20 * Math.pow(x, 2) + 30 * x + 40;
+        }
+
+        for (let i = 0; i < 10; i++) {
+            let x = Math.random();
+            let y = getY(x);
+            train(tf.scalar(x), tf.scalar(y));
+        }
+        let endY = predict(tf.scalar(10));
+        console.log('endY', endY.print());
+        console.log("endY", getY(10));
+
     },
 
     start() {
